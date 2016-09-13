8a0e1fdf5583d2b6d1c3ffef039a743ec83a0f1f
zz <zz.at.field@gmail.com>
2016-09-14 00:52:19 +0800

    给fixture 加上module的scope, 修改basetest

--- tests/basetest.py
+++ tests/basetest.py
@@ -1,18 +1,24 @@
-import pytest
-
-
-ay = pytest.mark.asyncio
-
-
 class BaseTest:
     NEXT_PAGE_INFO = None
     THREAD_LIST_NUM = 20
+    BLOCK_0_IMAGE_URL = None
+    BLOCK_1_IMAGE_URL = None
 
     def test_page(self, page):
         assert page.has_next()
         print(page.next_page_info)
         assert page.next_page_info == self.NEXT_PAGE_INFO
 
+    def test_blocks_num(self, page):
+        thread_list = page.thread_list()
+        print(len(thread_list))
+        assert len(thread_list) == self.THREAD_LIST_NUM
 
-    def test_block(self, page):
-        pass
+
+    def test_first_block(self, page):
+        block = page.thread_list()[0]
+        assert block.image_url == self.BLOCK_0_IMAGE_URL
+
+    def test_second_block(self, page):
+        block = page.thread_list()[1]
+        assert block.image_url == self.BLOCK_1_IMAGE_URL
--- tests/test_adnmb.py
+++ tests/test_adnmb.py
@@ -5,32 +5,29 @@ import asyncio
 from tests.basetest import *
 
 
-async def x():
+async def prepare_page():
     island_switcher.detect_by_url('http://h.adnmb.com/api/t/106983')
-    print(island_switcher.island)
     p = await Page.from_url('http://h.adnmb.com/api/t/106983', page_num=1)
     return p
 
 
-@pytest.fixture()
+@pytest.fixture(scope='module')
 def page():
-    p = asyncio.get_event_loop().run_until_complete(x())
+    p = asyncio.get_event_loop().run_until_complete(prepare_page())
     return p
+
+
 
 class TestAdnmb(BaseTest):
     NEXT_PAGE_INFO = ('http://h.adnmb.com/api/t/106983', 2)
-    IMAGE_URL_1 = 'http://h-adnmb-com.n1.yun.tf:8999/Public/Upload/image/2016-05-14/57372d96e74c2.jpg'
-    IMAGE_URL_2 = 'http://h-adnmb-com.n1.yun.tf:8999/Public/Upload/image/2016-05-14/57373100c6017.png'
+    BLOCK_0_IMAGE_URL = 'http://h-adnmb-com.n1.yun.tf:8999/Public/Upload/image/2016-05-14/57372d96e74c2.jpg'
+    BLOCK_1_IMAGE_URL = None
     THREAD_LIST_NUM = 21
 
-    def test_block(self, page):
-        thread_list = page.thread_list()
-        print(len(thread_list))
-        assert len(thread_list) == self.THREAD_LIST_NUM
-        block = thread_list[0]
-        assert block.image_url == self.IMAGE_URL_1
-        assert thread_list[1].image_url == None
-        assert thread_list[7].image_url == self.IMAGE_URL_2
+
+    def test_another_block(self, page):
+        block = page.thread_list()[7]
+        assert block.image_url == 'http://h-adnmb-com.n1.yun.tf:8999/Public/Upload/image/2016-05-14/57373100c6017.png'
 
 
 def atest_tmp(tmpdir):
@@ -42,5 +39,3 @@ def atest_tmp(tmpdir):
     print(a)
     assert 0
 
-
-# print(asyncio.get_event_loop().run_until_complete(x()))
