7bea454000021688109df941a2ef3838f9f74103
zz <zz.at.field@gmail.com>
2023-02-18 14:32:36 +0800

    remove useless code

--- island_backup/network.py
+++ island_backup/network.py
@@ -4,41 +4,9 @@ import aiohttp
 from .utils import EMPTY_DATA
 
 
-# async def get_data(url, callback=None, as_type='json', headers=None, retry=3):
-#     _retry = 0
-#     _base = 2
-#     _log_error = None
-
-#     while _retry < retry:
-#         try:
-#             async with session.get(url, headers=headers,proxy=proxy) as r:
-#                 data = await getattr(r, as_type)()
-#         except:
-#             # sleep 0s, 1s, 3s
-#             await asyncio.sleep(_base**_retry - 1)
-#             _retry += 1
-#             logging.debug('url: %s retry %s', url, _retry)
-#             _log_error = traceback.format_exc()
-#         else:
-#             break
-#     else:
-#         logging.error('\n Ignore Error:....\n%s\n...end\n', _log_error)
-#         print('Empyt data:',url)
-#         return EMPTY_DATA
-
-#     logging.debug('finish request %s', url)
-#     if callback:
-#         asyncio.create_task(callback(data, url))
-#     else:
-#         return data
-    
-
-
-
-
 class _Client:
     def __init__(self) -> None:
-        self._session:aiohttp.ClientSession = None
+        self._session:aiohttp.ClientSession = None  # type: ignore
         self.proxy = None
 
     async def get_data(self, url, as_type='json', headers=None) -> bytes|dict|object:
