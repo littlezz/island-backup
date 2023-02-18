6857e1a546efaf640cc1c042d0d9d42cff39ae79
zz <zz.at.field@gmail.com>
2023-02-18 14:29:26 +0800

    refacotor island switcher

--- island_backup/island_switcher.py
+++ island_backup/island_switcher.py
@@ -1,6 +1,7 @@
 from .islands.nimingban import NiMingBanPage
 from .islands.the4chan import The4ChanPage
 from .islands.the2chan import The2ChanPage
+from .islands.bases import BasePage
 
 
 island_class_map = {
@@ -12,18 +13,25 @@ island_class_map = {
 class IslandSwitcher:
     available_island = island_class_map.keys()
 
-    def __init__(self, island:str|None = None):
-        self.island = island
+    def __init__(self):
+        self._island:str|None = None
 
     def detect_by_url(self, url):
         for island in self.available_island:
             if island in url:
-                self.island = island
+                self._island = island
                 return
         raise ValueError('Unknown url: {}'.format(url))
+    
+    @property
+    def island(self)->str:
+        if self._island is not None:
+            return self._island
+        else:
+            raise
 
     @property
-    def island_page_model(self):
+    def island_page_model(self) -> BasePage:
         return island_class_map[self.island]
 
     def sanitize_url(self, url):
